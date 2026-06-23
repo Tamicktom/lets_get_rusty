b2b4a60f228434a96aa421837f2ecceec232c62f
Tamicktom <rickpokemon38@gmail.com>
2026-06-23 00:52:16 -0300

    hello world in rust

---
+++ hello_world/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // creation
+    let a: i16 = 5;
+
+    // mutability
+    let mut b = 5;
+    b = 10;
+    // shadowing
+    let c = 10;
+    let c = 20;
+
+    println!("Value of C: {c}");
+
+    // scope
+}
