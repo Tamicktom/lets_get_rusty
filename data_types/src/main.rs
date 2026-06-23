8f208d7fc056fd4efae095590a8050cab5475a29
Tamicktom <rickpokemon38@gmail.com>
2026-06-23 01:15:17 -0300

    Update main.rs

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -15,4 +15,35 @@ fn main() {
     let s3: i32 = 1;
     let s4: i64 = 1;
     let s5: i128 = 1;
+
+    // floating point numbers
+    let f1: f32 = 1.0;
+    let f2: f64 = 1.0;
+
+    // platform specific integers
+    let p1: usize = 1;
+    let p2: isize = 1;
+
+    // characters, &str and astring
+    let c1: char = 'c';
+    let c2: &str = "hello";
+    let c3: String = String::from("Hello");
+
+    // arrays
+    let a1: [i32; 5] = [1, 2, 3, 4, 5];
+    let i1: i32 = a1[4];
+
+    // tuples
+    let t1: (i32, i32, i32) = (1, 2, 3);
+    let t2: (i32, f64, &str) = (5, 5.0, "5");
+
+    let t3: i32 = t1.1;
+    let (t4, t5, t6) = t2;
+
+    let unit = ();
+
+    // Type aliasing
+    type Age = u8;
+
+    let a1: Age = 57;
 }
