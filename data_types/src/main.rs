3531d0c7f47500744c8baaccaf9664419639724a
Tamicktom <rickpokemon38@gmail.com>
2026-06-23 00:52:24 -0300

    some data types

---
+++ data_types/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // boolean
+    let b1: bool = true;
+
+    // unsigned integers
+    let i1: u8 = 1;
+    let i2: u16 = 1;
+    let i3: u32 = 1;
+    let i4: u64 = 1;
+    let i5: u128 = 1;
+
+    // singed integers
+    let s1: i8 = 1;
+    let s2: i16 = 1;
+    let s3: i32 = 1;
+    let s4: i64 = 1;
+    let s5: i128 = 1;
+}
